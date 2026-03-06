72cd61c4330afada91f23c07b59828f519d0cb98
da_b1rmuda <lamurka1337@yandex.ru>
2026-03-06 18:44:59 +0400

    update path-rush

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { RouterProvider } from "path-rush/react";
+import { RouterRoot } from "@da_b1rmuda/path-rush/react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<RouterProvider />
+		<RouterRoot />
 	</StrictMode>,
 );
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
+import { createPathRush } from "@da_b1rmuda/path-rush";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { pathRushRouting } from "path-rush/core";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), tailwindcss(), pathRushRouting()],
+	plugins: [
+		react(),
+		tailwindcss(),
+		createPathRush({
+			pagesDir: "src/pages",
+			pagesImportBase: "/src/pages",
+		}),
+	],
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
